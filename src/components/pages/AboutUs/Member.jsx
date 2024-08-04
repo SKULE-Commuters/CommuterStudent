b55be803588e9122965d26be49a2d3c2bb04ccab
if3ll <sunnwydays@gmail.com>
2024-08-04 14:29:59 -0400

    refactored Member card to map social media

--- src/components/pages/AboutUs/Member.jsx
+++ src/components/pages/AboutUs/Member.jsx
@@ -1,30 +1,29 @@
 import { LinkedInLogo, InstagramLogoBlue as InstagramLogo, YoutubeLogo, GithubLogo } from "../../../assets";
 
+const SocialMedia = [
+    {name: "LinkedIn", logo: LinkedInLogo},
+    {name: "Instagram", logo: InstagramLogo},
+    {name: "Youtube", logo: YoutubeLogo},
+    {name: "Github", logo: GithubLogo}
+];
+
 const Member = (props) => {
     return (
         <div className="member-box">
             <img className="member-pic" src={props.image} alt="Loading.."/>
             <h3>{props.name}</h3>
-            <h4>({props.pronouns})</h4>
+            <h4>{props.pronouns}</h4>
             <h5>{props.discipline}</h5>
             <h4>{props.role}</h4>
             <p>Fun Fact: {props.funfact}</p>
-            { props.linkedin === undefined
-                ? <></>
-                : <a href={props.linkedin} class="button"><img className="social-media-logo" src={LinkedInLogo} alt=""/></a>
-            }
-            { props.instagram === undefined
-                ? <></>
-                : <a href={props.instagram} class="button"><img className="social-media-logo" src={InstagramLogo} alt=""/></a>
-            }
-            { props.youtube === undefined
-                ? <></>
-                : <a href={props.youtube} class="button"><img className="social-media-logo" src={YoutubeLogo} alt=""/></a>
-            }
-            { props.github === undefined
-                ? <></>
-                : <a href={props.github} class="button"><img className="social-media-logo" src={GithubLogo} alt=""/></a>
-            }
+            {SocialMedia.map((media) => {
+                const url = props[media.name.toLowerCase()];
+                return url ? (
+                    <a href={url} className="button" key={media.name}>
+                        <img className="social-media-logo" src={media.logo} alt={`${media.name} logo`} />
+                    </a>
+                ) : null;
+            })}
         </div>
     );
 }
