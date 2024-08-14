394d94bb31d80e3737b579c3aa59b1a9918985db
if3ll <sunnwydays@gmail.com>
2024-08-14 00:08:57 -0400

    Widen transit map dropdowns

--- src/components/Dropdown/Dropdowns.jsx
+++ src/components/Dropdown/Dropdowns.jsx
@@ -1,16 +1,17 @@
 import './Dropdowns.css';
 
 const Dropdowns = (props) => {
+    /* Check if any child Dropdown contains map property */
+    const isMapDropdowns = props.children.some(child => child.props.map);
 
     return (
-        <div className="dropdowns">
+        <div className="dropdowns" style={isMapDropdowns ? { maxWidth: "75rem" } : {}}>
             {props.children.length !== 0 ?
                 props.children :
                 <p style={{ color: "#636363", fontStyle: "italic" }}>No results for the specified filters.</p>
             }
         </div>
-    )
-
-}
+    );
+};
 
 export default Dropdowns;
