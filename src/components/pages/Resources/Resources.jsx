fefd4a7f14b4e93e317da742defc6541ea46038b
if3ll <sunnwydays@gmail.com>
2024-08-14 12:27:30 -0400

    Add instagram to other info in resources

--- src/components/pages/Resources/Resources.jsx
+++ src/components/pages/Resources/Resources.jsx
@@ -134,7 +134,10 @@ const Resources = () => {
 
         <h1>Other Info</h1>
         <p>Explore more Skule Commuter resources at our <a href="https://linktr.ee/skule.commuters">LinkTree</a>. You can find our social links there along with information on joining the Skule Commuter Directorship team (when applicable).</p>
-        <p>You'll find up-to-date announcements in our <a href="https://discord.com/invite/yph2EjkbhW">Discord server</a> too.</p>
+        <p>
+          You'll find up-to-date announcements in our <a href="https://discord.com/invite/yph2EjkbhW">Discord server</a>.<br/>
+          If you like updates on Instagram, follow us on <a href="https://www.instagram.com/skule.commuters/">@skule.commuters</a>.
+        </p>
       </div>
     </Background>
   );
