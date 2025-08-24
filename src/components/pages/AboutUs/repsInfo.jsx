ad4645b6041dae3faf89511ec95dfff4f5ba2eb2
gfaus <gbgpf8@gmail.com>
2025-08-24 09:59:04 -0400

    info: Updated reps list to current as specified by Keting (only Alex)

--- src/components/pages/AboutUs/repsInfo.jsx
+++ src/components/pages/AboutUs/repsInfo.jsx
@@ -2,42 +2,12 @@ import { Michael, Eshaa, Viv, Celin } from '../../../assets';
 
 export const team = [
   {
-    name: "Michael",
+    name: "Alex",
     pronouns: "He/Him",
-    discipline: "CIV 2T7",
+    discipline: "EngSci 2T8",
     role: "2nd Year Representative",
-    image: Michael,
-    funfact: "If you take the Yonge side of Line 1 from the north, it's probably faster to get off at College or Wellesley and walk than it is to get to Queen's Park",
-    instagram: "https://www.instagram.com/m.hldr/"
-  },
-  {
-    name: "Eshaa Chenthuran",
-    pronouns: "She/Her",
-    discipline: "ECE 2T6",
-    role: "3rd Year Representative",
-    image: Eshaa,
-    funfact: "I broke my arm playing basketball once and have never played since 💀.",
-    linkedin: "https://ca.linkedin.com/in/eshaac",
-    github: "https://github.com/eshaachenthuran"
-  },
-  {
-    name: "Viv Huynh",
-    pronouns: "She/Her",
-    discipline: "Mech 2T4",
-    role: "4th Year Representative",
-    image: Viv,
-    funfact: "I make keyboards!",
-    linkedin: "http://linkedin.com/in/viv-huynh"
-  },
-  {
-    name: "Celin Begeshev",
-    pronouns: "She/They",
-    discipline: "Mech 2T5",
-    role: "PEY Representative",
-    image: Celin,
-    funfact: "I was the ultimate f!rosh back when I was a first year",
-    instagram: "https://www.instagram.com/celi_bean/"
-  },
+    funfact: "I have been on à Shinkansen and TGV in the same calendar year!",
+  }
   // {
   //   name: "",
   //   pronouns: "",
